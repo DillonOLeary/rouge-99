7d4a620c5c02828013a8710f6d25b573189e8168
Dillon O'Leary <doleary@wisc.edu>
2025-01-19 12:02:15 -0500

    Refactor handler

--- api-carbon-monitor/__main__.py
+++ api-carbon-monitor/__main__.py
@@ -1,6 +1,19 @@
+import os
+
 from api_handler import APIHandler
+from carbon_decorators import APIConsumptionManagement
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
 
 if __name__ == "__main__":
-    print(APIHandler.send_open_ai_chat_message("write a haiku about ai").choices[0].message)
-    print(APIHandler.send_open_ai_chat_message("write a haiku about ai").choices[0].message)
-    print(APIHandler.send_open_ai_chat_message("write a haiku about ai").choices[0].message)
+
+    api_tracker = APIConsumptionManagement()
+    client = OpenAI(
+        api_key=os.getenv("OPEN_AI_API_KEY")
+    )
+    handler = APIHandler(api_tracker, client)
+    handler.send_open_ai_chat_message("write a haiku about ai").choices[0].message
+    handler.send_open_ai_chat_message("write a haiku about ai").choices[0].message
+    print(handler.send_open_ai_chat_message("write a haiku about ai").choices[0].message)
--- api-carbon-monitor/api_handler.py
+++ api-carbon-monitor/api_handler.py
@@ -1,20 +1,13 @@
-import os
-
-from carbon_decorators import APIConsumptionManagement
-from dotenv import load_dotenv
-from openai import OpenAI
-
-load_dotenv()
-
-client = OpenAI(
-  api_key=os.getenv("OPEN_AI_API_KEY")
-)
-api_tracker = APIConsumptionManagement()
-
 class APIHandler:
-    @api_tracker.track_calls("send_open_ai_chat_message")
-    def send_open_ai_chat_message(message_content):
-        return client.chat.completions.create(
+    def __init__(self, api_tracker, client):
+        self.api_tracker = api_tracker
+        self.client = client
+        self.send_open_ai_chat_message = self.api_tracker.track_calls("send_open_ai_chat_message")(
+            self.send_open_ai_chat_message
+        )
+    
+    def send_open_ai_chat_message(self, message_content):
+        return self.client.chat.completions.create(
             model="gpt-3.5-turbo",
             store=True,
             messages=[
